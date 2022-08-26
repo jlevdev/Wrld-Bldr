b046f2672af914cc98a4ecbee3b0ccf6c6340bc6
James LeVasseur <jlevdev@gmail.com>
2022-08-25 21:45:05 -0400

    filled out login and register with bad styles

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,33 +1,42 @@
-import { Box, Button, Container, FormControl, FormControlLabel, FormHelperText, Grid, Input, InputLabel, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
 
 function Register() {
   let navigate = useNavigate();
 
   return (
     <div id="Register">
-      <Container sx={{ paddingTop: '30vh' }}>
+      <Container sx={{ paddingTop: "30vh" }}>
         <Box
-
           sx={{
-            backgroundColor: "#fff", paddingTop: 2, paddingBottom: 2,
-            margin: '0 auto', textAlign: 'center', borderRadius: '2%'
+            backgroundColor: "#fff",
+            margin: "0 auto",
+            textAlign: "center",
+            borderRadius: "2%",
+            width: "fit-content",
+            p: 2,
           }}
         >
-
           <form>
-            <Grid container rowSpacing={2} direction="column"
+            <Grid
+              container
+              rowSpacing={2}
+              direction="column"
               alignItems="center"
               justifyContent="center"
             >
-              <Grid item >
-                <Typography variant="h3">
-                  Sign Up!
-                </Typography>
+              <Grid item>
+                <Typography variant="h3">Sign Up!</Typography>
               </Grid>
-              <Grid item >
+              <Grid item>
                 <TextField
                   id="email-input"
                   name="email"
@@ -36,28 +45,29 @@ function Register() {
                   required
                 />
               </Grid>
-              <Grid item >
+              <Grid item>
                 <TextField
                   id="password-input"
                   name="password"
                   label="Password"
                   type="password"
                   required
-                /></Grid>
-              <Grid item >
+                />
+              </Grid>
+              <Grid item>
                 <TextField
                   id="password-input"
                   name="password"
                   label="Confirm Password"
                   type="password"
                   required
-                /></Grid>
-              <Grid item >
+                />
+              </Grid>
+              <Grid item>
                 <Button variant="contained" color="primary" type="submit">
                   Submit
                 </Button>
               </Grid>
-
             </Grid>
           </form>
         </Box>
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,4 +1,11 @@
-import { Box, Button, Container, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -7,7 +14,55 @@ function SignIn() {
 
   return (
     <div id="SignIn">
-      <Container sx={{ paddingTop: "35vh" }}></Container>
+      <Container sx={{ paddingTop: "30vh" }}>
+        <Box
+          sx={{
+            backgroundColor: "#fff",
+            margin: "0 auto",
+            textAlign: "center",
+            borderRadius: "2%",
+            width: "fit-content",
+            p: 2,
+          }}
+        >
+          <form>
+            <Grid
+              container
+              rowSpacing={2}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <Grid item>
+                <Typography variant="h3">Sign In</Typography>
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="email-input"
+                  name="email"
+                  label="Email"
+                  type="email"
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <TextField
+                  id="password-input"
+                  name="password"
+                  label="Password"
+                  type="password"
+                  required
+                />
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary" type="submit">
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Box>
+      </Container>
     </div>
   );
 }
