22dd54d509717070b37c77e5184d7435bf473478
James LeVasseur <jlevdev@gmail.com>
2022-08-26 18:07:46 -0400

    part way through jwt setup - pushing back to tower

--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('wrldbldr.urls', namespace='wrldbldr')),
     path('api/', include('wrldbldr_api.urls', namespace='wrldbldr_api')),
+    path('api-auth/', include('rest_framework.urls', namespace='restframework_api')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- wrldbldr_api/urls.py
+++ wrldbldr_api/urls.py
@@ -7,5 +7,5 @@ app_name = 'wrldbldr_api'
 urlpatterns = [
     path('settlement/<int:pk>/', SettlementDetail.as_view(), name='detailcreate'),
     path('settlement/', SettlementList.as_view(), name="listcreate"),
-    path('location/', LocationList.as_view(), name="listcreate"),
+    path('location/', LocationList.as_view(), name="listcreate")
 ]
